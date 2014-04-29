fc03532816053023ba7364e7773942960a0a2780
satsuma0711 <piano88>
2014-04-29 22:54:59 +0900

    【新規追加】機能追加

--- FundMaster/FundList.cs
+++ FundMaster/FundList.cs
@@ -51,12 +51,47 @@ namespace FundMaster
         {
             if (comboBox2.Text == "[投]申請書")
             {
-                makeApplicationForm();
+
+                for (int i = 0; i < this.dataGridView1.RowCount; i++)
+                {
+                    DataGridViewRow vr;
+                    vr = dataGridView1.Rows[i];
+                    makeApplicationForm2(vr);
+                }
             }
         }
 
-        private void makeApplicationForm()
+        private void makeApplicationForm2(DataGridViewRow dgvr)
         {
+            object oMissing = System.Reflection.Missing.Value;
+
+            Word._Application oWord;
+            Word._Document oDocument;
+            oWord = new Word.Application();
+            oWord.Visible = false;
+
+            oWord.DisplayAlerts = Word.WdAlertLevel.wdAlertsNone;
+
+            object oTemplate = "C:\\Users\\masuda\\Desktop\\申請書.dotx";
+            oDocument = oWord.Documents.Open(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
+
+            object fileName = @"C:\Users\masuda\Desktop\申請書_" + dgvr.Cells[0].Value.ToString() + ".docx";
+            oDocument.SaveAs(ref fileName, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                            ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                            ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+            
+            
+            oWord.Quit(Word.WdSaveOptions.wdDoNotSaveChanges);
+            oWord = null;
+
+            this.Close();
+        }
+
+        private void makeApplicationForm(DataGridViewRow dgvr)
+        {
+
+
+
             object oMissing = System.Reflection.Missing.Value;
 
             Word._Application oWord;
@@ -64,12 +99,64 @@ namespace FundMaster
             oWord = new Word.Application();
             oWord.Visible = true;
 
-            object oTemplate = "C:\\申請書.dot";
-            oDocument = oWord.Documents.Add(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
+            object oTemplate = "C:\\Users\\masuda\\Desktop\\申請書.doc";
+            oDocument = oWord.Documents.Open(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
 
             // 申請年月日
-            object oBookMark = "ProposedDate";
-            oDocument.Bookmarks.get_Item(ref oBookMark).Range.Text = "システムからの入力です。";
+            DateTime dt = DateTime.Today;
+            object oProposedDate = "ProposedDate";
+            oDocument.Bookmarks.get_Item(ref oProposedDate).Range.Text = dt.ToLongDateString();
+
+            // 件名
+            object oSubject = "Subject";
+            oDocument.Bookmarks.get_Item(ref oSubject).Range.Text = "";
+
+            // 件名
+            object oFundName1 = "FundName1";
+            oDocument.Bookmarks.get_Item(ref oFundName1).Range.Text = dgvr.Cells[0].Value.ToString();
+
+            // 委託会社
+            object oSettlorCompany = "SettlorCompany";
+            oDocument.Bookmarks.get_Item(ref oSettlorCompany).Range.Text = dgvr.Cells[1].Value.ToString();
+
+            // 名称
+            object oFundName2 = "FundName2";
+            oDocument.Bookmarks.get_Item(ref oFundName2).Range.Text = dgvr.Cells[0].Value.ToString();
+
+            // 販売開始日
+            object oSaleStartDate = "SaleStartDate";
+            oDocument.Bookmarks.get_Item(ref oSaleStartDate).Range.Text = "";
+
+            // 手数料(ネット)
+            object oNetCommission = "NetCommission";
+            oDocument.Bookmarks.get_Item(ref oNetCommission).Range.Text = "";
+
+            // 手数料(対面)
+            object oCounterCommission = "CounterCommission";
+            oDocument.Bookmarks.get_Item(ref oCounterCommission).Range.Text = "";
+
+            // 商品性
+            object oMarketability = "Marketability";
+            oDocument.Bookmarks.get_Item(ref oMarketability).Range.Text = dgvr.Cells[2].Value.ToString();
+
+            //oWord.Documents[@"C:\Users\masuda\Desktop\申請書_" + dgvr.Cells[0].Value.ToString() + ".doc"].Save();
+
+            try
+            {
+                object fileName = @"C:\Users\masuda\Desktop\申請書_" + dgvr.Cells[0].Value.ToString() + ".doc";
+                oWord.ActiveDocument.SaveAs(ref fileName, ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                            ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                            ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+            oWord.Quit(Word.WdSaveOptions.wdDoNotSaveChanges);
+            oWord = null;
 
             this.Close();
         }
